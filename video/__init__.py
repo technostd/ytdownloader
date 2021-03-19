538087fbd46726b8654b42eb397acb76d2f0d2e0
technostd <mr.mergel@bk.ru>
2021-03-19 19:44:54 +0300

    commit

--- video/__init__.py
+++ video/__init__.py
@@ -2,10 +2,30 @@ import pytube
 import time
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 
-yt = pytube.YouTube('https://www.youtube.com/watch?v=shyWnNdtqqM')
-name = 'downloaded_' + str(time.time()).replace('.', '_')
-print(name)
-yt.streams.filter(progressive=True, file_extension='mp4').desc().first().download(output_path='D:\youtube',
-                                                                                  filename=name + '.')
-ffmpeg_extract_subclip(f"D:\youtube\{name}.mp4", 61, 360, targetname="output.mp4")
+_path_ = 'C:\Files\ytdownloader'
 
+
+class Video:
+
+    def __init__(self, url, time_borders=''):
+        self.youTubeURL = url
+        self.timeBorders = time
+        self.currentTime = str(time.time()).replace('.', '_')
+
+    def download(self):
+        downloading_video = pytube.YouTube(self.youTubeURL)
+        filename_downloaded = 'downloaded_' + self.currentTime
+        downloading_video.streams.filter(progressive=True, file_extension='mp4') \
+            .desc() \
+            .first() \
+            .download(output_path=__path__, filename=filename_downloaded + '.')
+
+
+    def check_url(self):
+
+
+
+
+filenameCut = 'cut_' + currentTime
+
+ffmpeg_extract_subclip(f"video/cutted{name}.mp4", 61, 360, targetname=f"{name}.mp4")
