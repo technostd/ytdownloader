12cff48f73716fa531133e797cf36b62f7dae359
technostd <mr.mergel@bk.ru>
2021-04-02 19:44:00 +0300

    commit

--- bot/templates/keyboard.py
+++ bot/templates/keyboard.py
@@ -1,10 +1,30 @@
-from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+class Action:
+    TYPE_TEXT = 'text'
+    TYPE_OPEN_LINK = 'open_link'
+    TYPE_LOCATION = 'location'
+    TYPE_VK_PAY = 'vkpay'
+    TYPE_VK_APPS = 'open_app'
+    TYPE_CALLBACK = 'callback'
+
+    def __init__(self, type: str = TYPE_TEXT, payload=None, **kwargs):
 
 
-class Keyboard(VkKeyboard):
+class Button:
+    COLOR_PRIMARY = 'primary'
+    COLOR_SECONDARY = 'secondary'
+    COLOR_NEGATIVE = 'negative'
+    COLOR_POSITIVE = 'positive'
 
-    def __init__(self, keyboard):
-        super().__init__()
+    def __init__(self, action: Action, color=COLOR_PRIMARY):
+        self.action = action
+        self.color = color
 
 
+class Keyboard:
 
+    def __init__(self, inline: bool = False, one_time: bool = False):
+        self.keyboard = {
+            'inline': inline,
+            'one_time': one_time,
+            'buttons': [[]]
+        }
